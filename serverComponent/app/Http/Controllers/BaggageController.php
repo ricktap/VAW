84c14ea51234970d15f6fd645920b54f98f326fd
Patrick Selge <patrick.selge@comulus.io>
2015-06-12 10:43:26 +0200

    baggage creation implemented

--- serverComponent/app/Http/Controllers/BaggageController.php
+++ serverComponent/app/Http/Controllers/BaggageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -17,60 +18,32 @@ class BaggageController extends Controller
      */
     public function index()
     {
-        print_r(Baggage::all());
+        $baggages = Baggage::all();
+        return response()->json($baggages->toArray());
     }
 
     /**
      * Store a newly created resource in storage.
      *
+     * @param Request $request
      * @return Response
      */
-    public function store()
+    public function store(Request $request)
     {
-        echo "test";
-    }
+        // guard the passenger_id field
+        if (!$request->has('passenger_id')) {
+            return response("No passenger id provided", 400);
+        }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        // create a new baggage
+        $passengerId = $request->input('passenger_id');
+        $baggage = Baggage::create(["passenger_id" => $passengerId]);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function update($id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
+        // return the new baggage if it was created
+        if ($baggage) {
+            return response()->json($baggage->toArray());
+        }
+        // return server error, if creation was not possible
+        return response("Baggage was not created", 500);
     }
 }
