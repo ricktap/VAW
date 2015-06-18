f9d6db64c686215e1cab0dbbcf5392fe50267f50
Patrick Selge <patrick.selge@comulus.io>
2015-06-18 11:09:37 +0200

    switch to remote redis

--- socketServer/socket.js
+++ socketServer/socket.js
@@ -2,7 +2,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
-var redis = new Redis();
+var redis = new Redis({
+	port: 6379, 
+	host: 'cklio.com',
+	db: 0
+});
 
 redis.subscribe('baggage-channel', function(err, count) {
 
