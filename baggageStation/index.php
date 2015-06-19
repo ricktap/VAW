99444a0f28b42f95acc4e4d3c375fa7228267a7d
Patrick Selge <patrick.selge@comulus.io>
2015-06-19 18:34:24 +0200

    create next masks (5.5 out of 11)

--- baggageStation/index.php
+++ baggageStation/index.php
@@ -9,22 +9,43 @@ $app = new \Slim\Slim([
 $app->contentType('text/html; charset=utf-8');
 
 $view = $app->view();
-// $view->parserDirectory = 'vendor/slim';
-
-$view->parserOptions = array(
-    'charset' => 'utf-8',
-);
 
 $view->parserExtensions = array(
     new \Slim\Views\TwigExtension(),
 );
 
 $app->get('/passenger/:id', 'VAW\Controller\IndexController:run');
+
+
 $app->get('/login', function () use ($app) {
     $app->render('login.html');
 });
+
+
 $app->get('/passengerDataConfirm', function () use ($app) {
     $app->render('passenger-data-confirm.html');
 });
+
+
+$app->get('/dropBaggage', function () use ($app) {
+    $weight = rand(1, 60);
+    if ($app->request->params('weight')) {
+        $weight = $app->request->params('weight');
+    }
+
+    $app->render('drop-baggage.html', [
+        "weight" => $weight,
+        "allowedWeight" => 30
+    ]);
+});
+
+$app->get('/anotherBag', function () use ($app) {
+    $app->render('another-bag.html');
+});
+
+$app->get('/decideCustoms', function () use ($app) {
+    $app->render('customs-decide.html');
+});
+
 
 $app->run();
